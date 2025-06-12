569261be0a1c35ebbb7803a122d15a14366d32e6
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2025-06-12 15:11:19 +0200

    Adding work experience and info slides, as well as updating my js to support them
    Still lacking good descriptions on my work projects though.

--- js/Homepage.js
+++ js/Homepage.js
@@ -42,58 +42,53 @@ function Update()
 		{
 			openSlide = -1;
 		}
-		// card 0 BIOSIDE
-		if(elementMouseIsOver.id == "cardBioside" && clicked == true)
-		{
-			infoSlides[0].style.display = "block";
-			openSlide = 0;
-		}
-		else if ((!(elementMouseIsOver == infoSlides[0] || elementMouseIsOver.parentNode == infoSlides[0]) && clicked == true) || openSlide != 0)
-		{
-			document.getElementById("videoBioside").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-			infoSlides[0].style.display = "none";
-		}
-		
-		// card 1 Shattered Lights
-		if(elementMouseIsOver.id == "cardShatteredLights" && clicked == true)
-		{
-			openSlide = 1;
-			infoSlides[1].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[1] || elementMouseIsOver.parentNode == infoSlides[1]) && clicked == true)  || openSlide != 1)
-		{
-			infoSlides[1].style.display = "none";
-			document.getElementById("videoShatteredLights").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-		}
-		
-		// card 2 BaBooms
-		if(elementMouseIsOver.id == "cardBabooms" && clicked == true)
-		{
-			openSlide = 2;
-			infoSlides[2].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[2] || elementMouseIsOver.parentNode == infoSlides[2]) && clicked == true)  || openSlide != 2)
+        // card 0 Division
+        if(elementMouseIsOver.id == "cardDivision" && clicked == true)
+			{
+				infoSlides[0].style.display = "block";
+				openSlide = 0;
+			}
+			else if ((!(elementMouseIsOver == infoSlides[0] || elementMouseIsOver.parentNode == infoSlides[0]) && clicked == true) || openSlide != 0)
+			{
+				infoSlides[0].style.display = "none";
+			}
+	
+		// card 1 Warlords
+        if(elementMouseIsOver.id == "cardWarlords" && clicked == true)
+			{
+				infoSlides[1].style.display = "block";
+				openSlide = 1;
+			}
+			else if ((!(elementMouseIsOver == infoSlides[1] || elementMouseIsOver.parentNode == infoSlides[1]) && clicked == true) || openSlide != 1)
+			{
+				infoSlides[1].style.display = "none";
+			}
+	
+		// card 2 Avatar
+        if(elementMouseIsOver.id == "cardAvatar" && clicked == true)
+	    {
+		    infoSlides[2].style.display = "block";
+		    openSlide = 2;
+	    }
+		else if ((!(elementMouseIsOver == infoSlides[2] || elementMouseIsOver.parentNode == infoSlides[2]) && clicked == true) || openSlide != 2)
 		{
 			infoSlides[2].style.display = "none";
 		}
-		
-		
-		// card 1 TIFRAME
-		if(elementMouseIsOver.id == "cardTiframe" && clicked == true)
+
+		// card 3 BIOSIDE
+		if(elementMouseIsOver.id == "cardBioside" && clicked == true)
 		{
-			openSlide = 3;
 			infoSlides[3].style.display = "block";
+			openSlide = 3;
 		}
-		else if ((!(elementMouseIsOver == infoSlides[3] || elementMouseIsOver.parentNode == infoSlides[3]) && clicked == true)  || openSlide != 3)
+		else if ((!(elementMouseIsOver == infoSlides[3] || elementMouseIsOver.parentNode == infoSlides[3]) && clicked == true) || openSlide != 3)
 		{
-			document.getElementById("videoTiFrame").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+			document.getElementById("videoBioside").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
 			infoSlides[3].style.display = "none";
 		}
 		
-		
-		
-		// card 4 Gamejams
-		if(elementMouseIsOver.id == "cardGameJams" && clicked == true)
+		// card 4 Shattered Lights
+		if(elementMouseIsOver.id == "cardShatteredLights" && clicked == true)
 		{
 			openSlide = 4;
 			infoSlides[4].style.display = "block";
@@ -101,6 +96,41 @@ function Update()
 		else if ((!(elementMouseIsOver == infoSlides[4] || elementMouseIsOver.parentNode == infoSlides[4]) && clicked == true)  || openSlide != 4)
 		{
 			infoSlides[4].style.display = "none";
+			document.getElementById("videoShatteredLights").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+		}
+		
+		// card 5 BaBooms
+		if(elementMouseIsOver.id == "cardBabooms" && clicked == true)
+		{
+			openSlide = 5;
+			infoSlides[5].style.display = "block";
+		}
+		else if ((!(elementMouseIsOver == infoSlides[5] || elementMouseIsOver.parentNode == infoSlides[5]) && clicked == true)  || openSlide != 5)
+		{
+			infoSlides[5].style.display = "none";
+		}
+
+		// card 6 Tiframe
+		if(elementMouseIsOver.id == "cardTiframe" && clicked == true)
+		{
+			openSlide = 6;
+			infoSlides[6].style.display = "block";
+		}
+		else if ((!(elementMouseIsOver == infoSlides[6] || elementMouseIsOver.parentNode == infoSlides[6]) && clicked == true)  || openSlide != 6)
+		{
+			document.getElementById("videoTiFrame").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+			infoSlides[6].style.display = "none";
+		}
+					
+		// card 7 Gamejams
+		if(elementMouseIsOver.id == "cardGameJams" && clicked == true)
+		{
+			openSlide = 7;
+			infoSlides[7].style.display = "block";
+		}
+		else if ((!(elementMouseIsOver == infoSlides[7] || elementMouseIsOver.parentNode == infoSlides[7]) && clicked == true)  || openSlide != 7)
+		{
+			infoSlides[7].style.display = "none";
 		}
 	}
 	
