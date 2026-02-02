7f18de271320302666c0346dd55945f8b68e732c
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2026-02-02 10:49:08 +0100

    Updating website content, modifying the resume pdf

--- js/Homepage.js
+++ js/Homepage.js
@@ -10,6 +10,18 @@ var infoSlides;
 var mouseX = 0;
 var mouseY = 0;
 
+function CardDisplayModification(aCardNumber, anElementID)
+{
+	if(elementMouseIsOver.id == anElementID && clicked == true)
+	{
+		infoSlides[aCardNumber].style.display = "block";
+		openSlide = aCardNumber;
+	}
+	else if ((!(elementMouseIsOver == infoSlides[aCardNumber] || elementMouseIsOver.parentNode == infoSlides[aCardNumber]) && clicked == true) || openSlide != aCardNumber)
+	{
+		infoSlides[aCardNumber].style.display = "none";
+	}
+}
 
 function Init()
 {
@@ -42,97 +54,17 @@ function Update()
 		{
 			openSlide = -1;
 		}
-        // card 0 Division
-        if(elementMouseIsOver.id == "cardDivision" && clicked == true)
-			{
-				infoSlides[0].style.display = "block";
-				openSlide = 0;
-			}
-			else if ((!(elementMouseIsOver == infoSlides[0] || elementMouseIsOver.parentNode == infoSlides[0]) && clicked == true) || openSlide != 0)
-			{
-				infoSlides[0].style.display = "none";
-			}
-	
-		// card 1 Warlords
-        if(elementMouseIsOver.id == "cardWarlords" && clicked == true)
-			{
-				infoSlides[1].style.display = "block";
-				openSlide = 1;
-			}
-			else if ((!(elementMouseIsOver == infoSlides[1] || elementMouseIsOver.parentNode == infoSlides[1]) && clicked == true) || openSlide != 1)
-			{
-				infoSlides[1].style.display = "none";
-			}
-	
-		// card 2 Avatar
-        if(elementMouseIsOver.id == "cardAvatar" && clicked == true)
-	    {
-		    infoSlides[2].style.display = "block";
-		    openSlide = 2;
-	    }
-		else if ((!(elementMouseIsOver == infoSlides[2] || elementMouseIsOver.parentNode == infoSlides[2]) && clicked == true) || openSlide != 2)
-		{
-			infoSlides[2].style.display = "none";
-		}
-
-		// card 3 BIOSIDE
-		if(elementMouseIsOver.id == "cardBioside" && clicked == true)
-		{
-			infoSlides[3].style.display = "block";
-			openSlide = 3;
-		}
-		else if ((!(elementMouseIsOver == infoSlides[3] || elementMouseIsOver.parentNode == infoSlides[3]) && clicked == true) || openSlide != 3)
-		{
-			document.getElementById("videoBioside").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-			infoSlides[3].style.display = "none";
-		}
 		
-		// card 4 Shattered Lights
-		if(elementMouseIsOver.id == "cardShatteredLights" && clicked == true)
-		{
-			openSlide = 4;
-			infoSlides[4].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[4] || elementMouseIsOver.parentNode == infoSlides[4]) && clicked == true)  || openSlide != 4)
-		{
-			infoSlides[4].style.display = "none";
-			document.getElementById("videoShatteredLights").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-		}
-		
-		// card 5 BaBooms
-		if(elementMouseIsOver.id == "cardBabooms" && clicked == true)
-		{
-			openSlide = 5;
-			infoSlides[5].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[5] || elementMouseIsOver.parentNode == infoSlides[5]) && clicked == true)  || openSlide != 5)
-		{
-			infoSlides[5].style.display = "none";
-		}
-
-		// card 6 Tiframe
-		if(elementMouseIsOver.id == "cardTiframe" && clicked == true)
-		{
-			openSlide = 6;
-			infoSlides[6].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[6] || elementMouseIsOver.parentNode == infoSlides[6]) && clicked == true)  || openSlide != 6)
-		{
-			document.getElementById("videoTiFrame").contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-			infoSlides[6].style.display = "none";
-		}
-					
-		// card 7 Gamejams
-		if(elementMouseIsOver.id == "cardGameJams" && clicked == true)
-		{
-			openSlide = 7;
-			infoSlides[7].style.display = "block";
-		}
-		else if ((!(elementMouseIsOver == infoSlides[7] || elementMouseIsOver.parentNode == infoSlides[7]) && clicked == true)  || openSlide != 7)
-		{
-			infoSlides[7].style.display = "none";
-		}
+		CardDisplayModification(0, "cardDivision");
+		CardDisplayModification(1, "cardWarlords");
+		CardDisplayModification(2, "cardAvatar");
+		CardDisplayModification(3, "cardBioside");
+		CardDisplayModification(4, "cardShatteredLights");
+		CardDisplayModification(5, "cardBabooms");
+		CardDisplayModification(6, "cardTiframe");
+		CardDisplayModification(7, "cardUnannounced");
 	}
+
 	
 	clicked = false;
 }
